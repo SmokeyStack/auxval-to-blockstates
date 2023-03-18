eabda884f8f09fee4c381f6a0bdc973eafbe5900
SmokeyStack <seansyahril04@gmail.com>
2023-03-18 17:57:19 -0400

    Prepare for deployment!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
+    css: [
+        'vuetify/lib/styles/main.sass',
+        '@mdi/font/css/materialdesignicons.min.css'
+    ],
     build: {
-        transpile: ['vuetify'],
+        transpile: ['vuetify']
     },
     vite: {
         define: {
-            'process.env.DEBUG': false,
-        },
+            'process.env.DEBUG': false
+        }
     },
-})
+    app: {
+        baseURL: '/auxval-to-blockstates/'
+    }
+});
